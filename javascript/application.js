6d511eedb31170fa93f10053be61a4b41a4114e8
Hunter Fortuin <hunter@flitti.com>
2017-01-06 18:42:51 -0500

    adds responsive menu

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,64 @@
 $(document).ready(function() {
-	// Add JS Here
+	initializeDropdown();
+	initializeResizeListener();
 });
+
+function initializeResizeListener() {
+	if ($(window).width() >= 768) {
+		$('.desktop').removeClass('hidden');
+		$('.mobile').addClass('hidden');
+	} else {
+		$('.desktop').addClass('hidden');
+		$('.mobile').removeClass('hidden');
+	}
+
+	$(window).resize(function() {
+		if ($(window).width() >= 768) {
+			$('.desktop').removeClass('hidden');
+			$('.mobile').addClass('hidden');
+		} else {
+			$('.desktop').addClass('hidden');
+			$('.mobile').removeClass('hidden');
+		}
+	});
+}
+
+function initializeDropdown() {
+	var menu = document.getElementById('menu'),
+		WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+
+	function toggleHorizontal() {
+		[].forEach.call(
+			document.getElementById('menu').querySelectorAll('.custom-can-transform'),
+			function(el){
+				el.classList.toggle('pure-menu-horizontal');
+			}
+		);
+	};
+
+	function toggleMenu() {
+		// set timeout so that the panel has a chance to roll up
+		// before the menu switches states
+		if (menu.classList.contains('open')) {
+			setTimeout(toggleHorizontal, 500);
+		}
+		else {
+			toggleHorizontal();
+		}
+		menu.classList.toggle('open');
+		document.getElementById('toggle').classList.toggle('x');
+	};
+
+	function closeMenu() {
+			if (menu.classList.contains('open')) {
+					toggleMenu();
+			}
+	}
+
+	document.getElementById('toggle').addEventListener('click', function (e) {
+			toggleMenu();
+			e.preventDefault();
+	});
+
+	window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+}
